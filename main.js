081d693ab3610e049f90914f00b914d3c4a8df73
samuellvn <138622449+samuellvn@users.noreply.github.com>
2023-11-16 15:29:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,77 @@
+var musica_play1;
+var musica_play2;
+
+
 var canvas;
 var video;
-var posenet;
-var nariz_x;
-var nariz_y;
-var pulso_esq_x;
-var pulso_dir_x;
-var diferenca;
+var musica;
+var pose_net;
 
-function setup(){
-    canvas=createCanvas(500, 300);
-    canvas.position(560, 150);
+var pulso_esqX=0;
+var pulso_esqY=0;
+var pulso_dirX=0;
+var pulso_dirY=0;
+
+var score_pulso_esq=0;
+var score_pulso_dir=0;
+
+function preload(){
+    musica_play1=loadSound("music.mp3");
+    musica_play2=loadSound("music2.mp3");
+}
+
+function setup( ){
+    canvas=createCanvas(600, 500);
+    canvas.center();
     video=createCapture(VIDEO);
-    video.size(500, 300);
-    posenet=ml5.poseNet(video, model_loded);
-    posenet.on("pose", got_poses);
+    video.hide();
+    pose_net=ml5.poseNet(video, model_loaded);
+    pose_net.on("pose", got_poses);
 }
-function model_loded(){
-    console.log("posenet foi inicializado");
-}
+
 function got_poses(results){
     if(results.length>0){
         console.log(results);
-        nariz_x=results[0].pose.nose.x;
-        nariz_y=results[0].pose.nose.y;
-        pulso_esq_x=results[0].pose.leftWrist.x;
-        pulso_dir_x=results[0].pose.rightWrist.x;
-        diferenca=floor(pulso_esq_x-pulso_dir_x);
+        score_pulso_esq=results[0].pose.keypoints[9].score;
+        
+
+        pulso_esqX=results[0].pose.leftWrist.x;
+        pulso_esqY=results[0].pose.leftWrist.y;
+        pulso_dirX=results[0].pose.rightWrist.x;
+        pulso_dirY=results[0].pose.rightWrist.y;
+
+        console.log("valorX do pulso esquerdo: "+pulso_esqX);
+        console.log("valorY do pulso esquerdo: "+pulso_esqY);
+        console.log("valorX do pulso direito: "+pulso_dirX);
+        console.log("valorY do pulso direito: "+pulso_esqY);
     }
+
+
 }
+
+function model_loaded(){
+    console.log("pose net foi carregado");
+}
+
+function play(){
+    musica.play();
+    musica.setVolume(1);
+    musica.rate(1);
+
+}
+
 function draw(){
-   background("white");
-   textSize(diferenca);
-   fill("black")
-   text("sei que nada sei",pulso_dir_x=100,pulso_esq_x=100);
+    image(video, 0, 0, 600, 500);
+    fill("red");
+    stroke("red");
+    
+    //if(score_pulso_esq>0.2){
+        console.log("entrou");
+        circle(pulso_esqX, pulso_esqY, 20);
+        var numero_pulso_esq=Number(pulso_esqY);
+        var remove_decimais=floor(numero_pulso_esq);
+        var volume=remove_decimais/500;
+        document.getElementById("volume").innerHTML="volume: "+volume;
+        musica.setVolume(volume);
+    //}
 }
